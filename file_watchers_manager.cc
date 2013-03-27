f3cc9cb734342a2b201b4f454e98a52c7f259f18
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-03-27 21:56:07 +0100

    Implemented file_watchers_manager.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

---
+++ file_watchers_manager.cc
@@ -0,0 +1,61 @@
+/**
+ *  @author dmilith
+ *
+ *   © 2013 - VerKnowSys
+ *
+ */
+
+#include "syndir.h"
+#include "file_watcher.h"
+
+
+FileWatchersManager::FileWatchersManager(const QString& sourceDir, const QString& fullDestinationSSHPath) {
+    this->fullDestinationSSHPath = fullDestinationSSHPath;
+    this->files = scanDir(QDir(sourceDir));
+    QStringList sshDirPartial = fullDestinationSSHPath.split(":");
+    QStringList sshUserServerPartial = sshDirPartial.value(0).split("@"); /* first part is user@host */
+    this->remotePath = sshDirPartial.value(1); /* last one is a remote path */
+    this->userName = sshUserServerPartial.value(0);
+    this->hostName = sshUserServerPartial.value(1);
+
+    if (this->userName.isEmpty()) {
+        qDebug() << "No user name specified, trying to get it from ENV.";
+        this->userName = getenv("USER");
+    }
+
+    if (this->hostName.isEmpty() or this->userName.isEmpty()) {
+        qDebug() << "Invalid (empty) host or user name.";
+        usage();
+        exit(1);
+    }
+
+    qDebug() << "Total files:" << files->size();
+    qDebug() << "Traversing paths";
+
+    for (int i = 0; i < files->size(); ++i) {
+        auto entry = files->at(i);
+        qDebug() << "Entry:" << entry;
+        new FileWatcher(entry, this);
+    }
+}
+
+
+/* by tallica */
+const QStack<QString>* FileWatchersManager::scanDir(QDir dir) {
+    QStack<QString> *files = new QStack<QString>();
+    dir.setFilter(QDir::Files | QDir::NoDotAndDotDot | QDir::NoSymLinks); // QDir::Dirs | QDir::Hidden |
+    QDirIterator it(dir, QDirIterator::Subdirectories);
+
+    files->push(dir.absolutePath());
+
+    while (it.hasNext())
+        files->push(it.next());
+
+    return files;
+}
+
+
+void FileWatchersManager::fileChangedSlot(const QString& file) {
+    qDebug() << "File changed:" << file;
+
+}
---
+++ file_watchers_manager.h
@@ -0,0 +1,52 @@
+/**
+ *  @author dmilith
+ *
+ *   © 2013 - VerKnowSys
+ *
+ */
+
+#ifndef __FILE_WATCHERS_MANAGER_H__
+#define __FILE_WATCHERS_MANAGER_H__
+
+
+#include <QtCore>
+
+#include "syndir.h"
+// #include "file_watcher.h"
+#include "file_watchers_manager.h"
+
+
+class FileWatchersManager: public QFileSystemWatcher {
+    Q_OBJECT
+
+    public:
+        FileWatchersManager(const QString& sourceDir, const QString& fullDestinationSSHPath);
+        const QStack<QString>* scanDir(QDir dir);
+
+
+    private:
+        const QStack<QString> *files;
+        QString fullDestinationSSHPath;
+        QString remotePath;
+        QString userName;
+        QString hostName;
+
+    //     QString dataDir;
+    //     QString remotePath;
+    //     QString userName;
+    //     QString hostName;
+
+
+    // signals:
+
+    public slots:
+        void fileChangedSlot(const QString& file);
+
+    // public slots:
+        // void fileChangedSlot(const QString& file);
+        // void dirChangedSlot(const QString& dir);
+
+};
+
+
+#endif
